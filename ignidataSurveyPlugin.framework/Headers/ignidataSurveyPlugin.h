3134a0696696ad032168e7c6634fae225c00c449
andretbm <andremuchagata@gmail.com>
2015-11-13 10:51:19 +0000

    testing

---
+++ ignidataSurveyPlugin.framework/Headers/ignidataSurveyPlugin.h
@@ -0,0 +1,26 @@
+/*
+ Copyright (c) 2015 IGNIDATA Team - https://github.com/ignidata
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE. */
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ignidataSurveyPlugin.
+FOUNDATION_EXPORT double ignidataSurveyPluginVersionNumber;
+
+//! Project version string for ignidataSurveyPlugin.
+FOUNDATION_EXPORT const unsigned char ignidataSurveyPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ignidataSurveyPlugin/PublicHeader.h>
+
+
